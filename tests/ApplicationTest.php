6a6091d73b6bdcc5f4a3d8585d9d17249e81d571
elina <badianeelina8@gmail.com>
2019-08-05 00:39:59 +0200

    ajout des tests unitaires

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -6,8 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 class ApplicationTest extends TestCase
 {
-    public function testSomething()
+    public function testpartOk()
     {
-        $this->assertTrue(true);
+        $part = static::createClient([],[
+            'PHP_AUTH_USER' => 'ElinaAdminWari',
+            'PHP_AUTH_PW'   => 'marie199'
+
+        ]);
+        
+        $crawler = $part->request('POST', '/api/ajoutdestrois',[],[],
+        ['CONTENT_TYPE'=>"application/json"],
+        '{"ninea":"guey1994","adresse":"guediawaye","raison_sociale":"SA","photo":"image","nom":"gueye","prenom":"mame amy","username":"amy","password":"amy1994","roles":2}',
+        '{"ninea":"ngom","adresse":"kounoul","raison_sociale":"SA","photo":"image","nom":"ngom","prenom":"abdoulaye","username":"junior","password":"laye1992","roles":3}',
+        '{"ninea":"ngom","adresse":"hlm","raison_sociale":"SA","photo":"image","nom":"ngom","prenom":"jolie","username":"mariama","password":"maria1995","roles":4}',
+        '{"ninea":"dieme","adresse":"mbao","raison_sociale":"SA","photo":"image","nom":"dieme","prenom":"moussa","username":"morcar","password":"mos1995","roles":4}');
+        $rep=$part->getResponse();
+        var_dump($rep);
+        $this->assertSame(201,$part->getResponse()->getStatusCode());
     }
 }
--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -5,9 +5,19 @@ namespace App\Tests;
 use PHPUnit\Framework\TestCase;
 
 class ServiceTest extends TestCase
-{
-    public function testSomething()
+{public function testajoutOk()
     {
-        $this->assertTrue(true);
+        $ajout = static::createClient([],[
+            'PHP_AUTH_USER' => 'ElinaAdminWari',
+            'PHP_AUTH_PW'   => 'marie199'
+
+        ]);
+        
+        $crawler = $ajout->request('POST', '/api/ajoutargent',[],[],
+        ['CONTENT_TYPE'=>"application/json"],
+        '{"numerocompte":"0408201921540308pm","compte":4,"user":6,"montant":75000}');
+        $rep=$ajout->getResponse();
+        var_dump($rep);
+        $this->assertSame(201,$ajout->getResponse()->getStatusCode());
     }
 }
